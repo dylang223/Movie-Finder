f683aa850146e6e64f8f09cd8ec75e325763afad
dylang223 <glynndylan36@gmail.com>
2025-03-19 20:39:35 +0000

    added omdb service

--- src/app/services/omdb-a.service.ts
+++ src/app/services/omdb-a.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs';
+import { IOMDBResponse } from '../omdbresponse';
+
 @Injectable({
   providedIn: 'root'
 })
-export class OmdbAService {
+export class OmdbApiService {
 
-  constructor() { }
+  private _siteURL="https://www.omdbapi.com/";
+  private _key="?apikey=xxxxxxxx&t=";
+  constructor(private _http:HttpClient) { }
+
+  getMovieData(movieName:string):Observable<IOMDBResponse> {
+    return this._http.get<IOMDBResponse>(this._siteURL+ this._key + movieName)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.log('OmdbApiService:' + err.message);
+    //previous version return throwError("error : " + err.message);
+    return throwError(() => new Error("OmdbApiService:" + err.message))
+  }
+  
 }
