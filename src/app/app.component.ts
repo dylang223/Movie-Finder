13996c6aea9facdfec89e5c0ef066155bbf541fd
dylang223 <glynndylan36@gmail.com>
2025-03-19 21:34:38 +0000

    why wont this work bruh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IOMDBResponse } from './omdbresponse';   
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Movie-Finder';
+  title = 'Movie Finder';
+  movieData: IOMDBResponse | undefined;
+  errorMessage: any;
+
+  constructor(private _omdbService: OmdbApiService) { }
+
+  getMovieDetails(movieName: string): boolean {
+      this._omdbService.getMovieData(movieName).subscribe(
+          movieData => {
+              this.movieData = movieData;
+              console.log("Director name : " + this.movieData.Director);
+          }
+      )
+      return false;
+  }
 }
