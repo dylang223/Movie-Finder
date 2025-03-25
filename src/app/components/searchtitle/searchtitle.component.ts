0ce68187ef172327f67418ea175bd59428c5b01b
dylang223 <glynndylan36@gmail.com>
2025-03-25 14:49:36 +0000

    pusing

---
+++ src/app/components/searchtitle/searchtitle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchtitle',
+  imports: [],
+  templateUrl: './searchtitle.component.html',
+  styleUrl: './searchtitle.component.css'
+})
+export class SearchtitleComponent {
+
+}
