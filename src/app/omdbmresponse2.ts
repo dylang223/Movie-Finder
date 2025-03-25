d38724dcac3044128b253b7df390d007eea56064
dylang223 <glynndylan36@gmail.com>
2025-03-25 18:07:28 +0000

    commit type shit

---
+++ src/app/omdbmresponse2.ts
@@ -0,0 +1,15 @@
+
+export interface IOMDBResponse2 {
+    Search: movieDetails[];
+    totalResults: number;
+    Error: string;
+}
+
+interface movieDetails {
+    Title: string;
+    Year: string;
+    Director: string;
+    Poster: string;
+    Type: string;
+    Error: string;
+}
--- src/app/services/omdb-a.service.ts
+++ src/app/services/omdb-a.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { IOMDBResponse2 } from '../omdbmresponse2';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs';
@@ -12,6 +12,7 @@ export class OmdbApiService {
 
   private _siteURL="https://www.omdbapi.com/";
   private _key="?apikey=962eb4b2&t=";
+  private _key2="?apikey=962eb4b2&t=";
   constructor(private _http:HttpClient) { }
 
   getMovieData(movieName:string):Observable<IOMDBResponse> {
@@ -22,11 +23,20 @@ export class OmdbApiService {
     catchError(this.handleError)
     );
   }
+ 
 
   private handleError(err: HttpErrorResponse) {
     console.log('OmdbApiService:' + err.message);
     //previous version return throwError("error : " + err.message);
     return throwError(() => new Error("OmdbApiService:" + err.message))
+  }
+  getMoviesData(movieName:string, page:number):Observable<IOMDBResponse2> {
+    return this._http.get<IOMDBResponse2>(this._siteURL+ this._key2 + movieName + "&page=" + page)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
   }
   
 }
